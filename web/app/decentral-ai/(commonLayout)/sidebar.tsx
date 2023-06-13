162aaa2aa8754af5239a01e230ab5b9182ce437d
Ethan Yeh <ethanyeh0301@gmail.com>
2023-06-13 22:18:52 +0800

    feat: web nav active

--- web/app/decentral-ai/(commonLayout)/sidebar.tsx
+++ web/app/decentral-ai/(commonLayout)/sidebar.tsx
@@ -4,13 +4,18 @@ import { WorkspaceProvider } from "@/context/workspace-context";
 import NavItem from "./navItem";
 import WorkspaceNavItems from "./workspaceNavItems";
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 const Sidebar = () => {
+  const segment = useSelectedLayoutSegment();
   return (
-    <aside className="w-[60px] shrink-0 border-r border-gray-200" aria-label="Sidebar">
+    <aside
+      className="w-[60px] shrink-0 border-r border-gray-200"
+      aria-label="Sidebar"
+    >
       <div>
         <div className="flex flex-col items-center gap-4 pt-4 pb-7 border-b border-gray-200">
-          <NavItem>
+          {/* <NavItem>
             <svg
               width={24}
               height={24}
@@ -25,31 +30,32 @@ const Sidebar = () => {
                 fill="#4B5563"
               />
             </svg>
-          </NavItem>
-
-          <NavItem>
-            <svg
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
-              preserveAspectRatio="xMidYMid meet"
-            >
-              <g clip-path="url(#clip0_803_23580)">
-                <path
-                  d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM6.5 17.5L14.01 14.01L17.5 6.5L9.99 9.99L6.5 17.5ZM12 10.9C12.61 10.9 13.1 11.39 13.1 12C13.1 12.61 12.61 13.1 12 13.1C11.39 13.1 10.9 12.61 10.9 12C10.9 11.39 11.39 10.9 12 10.9Z"
-                  fill="#4B5563"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_803_23580">
-                  <rect width={24} height={24} fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
-          </NavItem>
+          </NavItem> */}
+          <Link href="/decentral-ai/explore/apps">
+            <NavItem active={segment === "explore"}>
+              <svg
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
+                preserveAspectRatio="xMidYMid meet"
+              >
+                <g clip-path="url(#clip0_803_23580)">
+                  <path
+                    d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM6.5 17.5L14.01 14.01L17.5 6.5L9.99 9.99L6.5 17.5ZM12 10.9C12.61 10.9 13.1 11.39 13.1 12C13.1 12.61 12.61 13.1 12 13.1C11.39 13.1 10.9 12.61 10.9 12C10.9 11.39 11.39 10.9 12 10.9Z"
+                    fill="#4B5563"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_803_23580">
+                    <rect width={24} height={24} fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </NavItem>
+          </Link>
         </div>
       </div>
       <div>
@@ -58,7 +64,7 @@ const Sidebar = () => {
             <WorkspaceNavItems />
           </WorkspaceProvider>
           <Link href="/decentral-ai/dao/create">
-            <NavItem>
+            <NavItem active={segment === "dao"}>
               <svg
                 width={24}
                 height={24}
--- web/app/decentral-ai/(commonLayout)/workspaceNavItems.tsx
+++ web/app/decentral-ai/(commonLayout)/workspaceNavItems.tsx
@@ -2,7 +2,7 @@ import { switchWorkspace } from "@/service/common";
 import { useTranslation } from "react-i18next";
 import { useContext } from "use-context-selector";
 import { ToastContext } from "@/app/components/base/toast";
-import { useRouter } from "next/navigation";
+import { useRouter, useSelectedLayoutSegment } from "next/navigation";
 import { useWorkspacesContext } from "@/context/workspace-context";
 import NavItem from "./navItem";
 import { useSWRConfig } from "swr";
@@ -13,7 +13,7 @@ const WorkspaceNavItems = () => {
   const { notify } = useContext(ToastContext);
   const { workspaces } = useWorkspacesContext();
   const { mutate } = useSWRConfig();
-
+  const segment = useSelectedLayoutSegment();
   const handleSwitchWorkspace = async (tenant_id: string) => {
     try {
       await switchWorkspace({ url: `/workspaces/switch`, body: { tenant_id } });
@@ -21,10 +21,10 @@ const WorkspaceNavItems = () => {
       mutate({ url: "/workspaces" });
       mutate({ url: "/apps", params: { page: 1, limit: 30 } });
       mutate({ url: "/datasets", params: { page: 1, limit: 30 } });
-      notify({
-        type: "success",
-        message: t("common.actionMsg.modifiedSuccessfully"),
-      });
+      // notify({
+      //   type: "success",
+      //   message: t("common.actionMsg.modifiedSuccessfully"),
+      // });
       router.push("/decentral-ai/overview");
     } catch (e) {
       notify({ type: "error", message: t("common.provider.saveFailed") });
@@ -36,7 +36,9 @@ const WorkspaceNavItems = () => {
     <>
       {workspaces.map((workspace) => (
         <NavItem
-          active={workspace.current}
+          active={
+            segment !== "dao" && segment !== "explore" && workspace.current
+          }
           onClick={() => {
             handleSwitchWorkspace(workspace.id);
           }}
